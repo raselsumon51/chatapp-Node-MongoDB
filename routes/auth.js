33829058663b827fe110c63531d9fa9d2cec6ac0
raselsumon51 <raselsumon51@gmail.com>
2024-06-08 03:00:42 +0600

    ui improved

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 
 router.get('/', (req, res) => {
-    res.render('index',{loggedIn:false});
+    res.render('login',{loggedIn:false});
 });
 
 router.get('/register', (req, res) => {
